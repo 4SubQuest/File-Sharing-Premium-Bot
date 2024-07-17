53d3bebf42c2c6d3966adfa96946f2264084c439
4SubQuest <abidabdullah1415@gmail.com>
2024-07-17 11:23:16 +0600

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -19,7 +19,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○   Oᴡɴᴇʀ - </b> <a href='tg://user?id={OWNER_ID}'>Mᴏɴᴋᴇʏ D Lᴜғғʏ</a> \n<b>○   Cʜᴀɴɴᴀʟ - </b> <a href='https://t.me/AnimeQuestX'>Aɴɪᴍᴇ Qᴜᴇsᴛ</a> \n<b>○   Hɪɴᴅɪ Cʜᴀɴɴᴀʟ - </b> <a href='https://t.me/AnimeQuestHindi'>Aɴɪᴍᴇ Qᴜᴇsᴛ Hɪɴᴅɪ</a> \n<b>○   Oɴɢᴏɪɴɢ Cʜᴀɴɴᴀʟ - </b> <a href='https://t.me/OngoingAnimeQuest'>Oɴɢᴏɪɴɢ Aɴɪᴍᴇ Qᴜᴇsᴛ</a> \n<b>○   Dɪsᴄᴜssᴛɪᴏɴ Gʀᴏᴜᴘ - </b> <a href='https://t.me/+r-x-wA4JT5gxZjVl'>Aɴɪᴍᴇ Qᴜᴇsᴛ Cʜᴀᴛ</a>",
+            text = <b>○   Oᴡɴᴇʀ - </b> <a href='tg://user?id={OWNER_ID}'>Mᴏɴᴋᴇʏ D Lᴜғғʏ</a> \n<b>○   Cʜᴀɴɴᴀʟ - </b> <a href='https://t.me/AnimeQuestX'>Aɴɪᴍᴇ Qᴜᴇsᴛ</a> \n<b>○   Hɪɴᴅɪ Cʜᴀɴɴᴀʟ - </b> <a href='https://t.me/AnimeQuestHindi'>Jᴏɪɴ Nᴏᴡ</a> \n<b>○   Oɴɢᴏɪɴɢ Cʜᴀɴɴᴀʟ - </b> <a href='https://t.me/OngoingAnimeQuest'>Jᴏɪɴ Nᴏᴡ</a> \n<b>○   Dɪsᴄᴜssᴛɪᴏɴ Gʀᴏᴜᴘ - </b> <a href='https://t.me/+r-x-wA4JT5gxZjVl'>Jᴏɪɴ Nᴏᴡ</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
